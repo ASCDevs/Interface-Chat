13743374756159921a731b5511bf056ec3bb998f
Alexandre Santos <ale.santos49@gmail.com>
2021-04-05 22:43:31 -0300

    Adição do começo da funcionalidade de carregar contatos

--- src/application/TelaPainelController.java
+++ src/application/TelaPainelController.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -10,6 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -24,6 +26,9 @@ public class TelaPainelController implements Initializable {
 	@FXML
 	public VBox pnContatos;
 	
+	public ArrayList<Contato> contatos;
+	
+	
 	@FXML
 	public void sairAction() {
 		System.exit(0);	
@@ -32,26 +37,65 @@ public class TelaPainelController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		Node[] contatos = new Node[20];
-		String cinza, selecionado;
-		cinza = "#EFEFEF";
-		selecionado = "#c9c9c9" ;
+		contatosFake();
+		carregarContatos();
+	}
+	
+	
+	public void carregarContatos() {
 		
-		for(int i=0;i<contatos.length;i++) {
+		Pane[] items = new Pane[contatos.size()];
+		String cinza, branco, selecionado;
+		cinza = "#EFEFEF";
+		branco = "#FFFFFF";
+		selecionado = "#e3e3e3" ;
+		
+		for(int i=0;i<items.length;i++) {
 			try {
-				contatos[i] = FXMLLoader.load(getClass().getResource("/resources/itemContato.fxml"));
-				if(i%2==0) {
-					contatos[i].setStyle("-fx-background-color: "+cinza);
-				}
+				final int j = i;
+				final String corFundo;
 				
-				pnContatos.getChildren().add(contatos[i]);
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/itemContato.fxml"));
+				loader.setController(contatos.get(i));
+				items[i] = loader.load();
+				
+			
+				if(i%2==0) {
+					corFundo = cinza;
+				}else {
+					corFundo = branco;
+				}
+				items[i].setStyle("-fx-background-color: "+corFundo);
+				items[i].setOnMouseEntered(event -> {
+					items[j].setStyle("-fx-background-color : "+selecionado);
+				});
+				items[i].setOnMouseExited(event -> {
+					items[j].setStyle("-fx-background-color: "+corFundo);
+				});
+				
+				pnContatos.getChildren().add(items[i]);
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-			
 		}
+	}
+	
+	
+	public void contatosFake() {
+		contatos = new ArrayList();
+		
+		contatos.add(new Contato("Ulisses Valente","online"));
+		contatos.add(new Contato("Noémi Junqueira","online"));
+		contatos.add(new Contato("Davide Paião","online"));
+		contatos.add(new Contato("Pérola Mexia ","online"));
+		contatos.add(new Contato("Catarina Noleto","online"));
+		contatos.add(new Contato("Anastacia Lobato","online"));
+		contatos.add(new Contato("Fernão Madureira","online"));
+		contatos.add(new Contato("André Ramos","offline"));
+		contatos.add(new Contato("Silvana Lameiras","offline"));
+		contatos.add(new Contato("Pérola Ruela","offline"));
+		contatos.add(new Contato("Emma Brito","offline"));
+		contatos.add(new Contato("Brayan Moreno","offline"));
 		
 	}
 }
